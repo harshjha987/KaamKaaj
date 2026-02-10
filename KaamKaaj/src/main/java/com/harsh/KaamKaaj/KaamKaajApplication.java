5ee1b9d4390c19939ace1a3c67564343ef3c2d29
harshjha987 <2440851@cognizant.com>
2026-02-10 17:07:10 +0530

    Initial commit

---
+++ KaamKaaj/src/main/java/com/harsh/KaamKaaj/KaamKaajApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.KaamKaaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaamKaajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaamKaajApplication.class, args);
+	}
+
+}
---
+++ KaamKaaj/src/test/java/com/harsh/KaamKaaj/KaamKaajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.KaamKaaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaamKaajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
